128715572341189ed528279911fbc751bc08908c
LEXASPB78 <ashkreba@yandex.ru>
2022-09-04 11:52:24 +0300

    Reshenie64

---
+++ Zadacha64/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 64: Задайте значения M и N. 
+Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 6, 7, 8""
+*/
+
+void PrintNumbers(int start, int end)
+{
+    if (start > end) return;
+    Console.Write($"{start} ");
+    PrintNumbers(start + 1, end);
+}
+
+Console.Write("Введите начальное любое целое число ");
+int startNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите конечное любое целое число ");
+int endNumber = Convert.ToInt32(Console.ReadLine());
+PrintNumbers(startNumber, endNumber);
